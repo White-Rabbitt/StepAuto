e8fb3add5c029ae1563d09921aa8324317ec9422
White_Rabbit <lacrimozzy989wr>
2021-08-04 23:07:31 +0300

    edit less

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,11 +1,55 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
-
-def test_abs2():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+from selenium import webdriver
+import time
+import unittest
 
 
-if __name__ == "__main__":
-    test_abs1()
-    test_abs2()
-    print("Everything passed")
+class test_reg_page(unittest.TestCase):
+    def test_registration_page_1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element_by_xpath("//input[@class='form-control SeleniumPython' and @required]")
+        input1.send_keys("Ivan")
+
+        input2 = browser.find_element_by_xpath("//input[@class='form-control second' and @required]")
+        input2.send_keys("Petrov")
+
+        input3 = browser.find_element_by_xpath("//input[@class='form-control third' and @required]")
+        input3.send_keys("Smolensk")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", welcome_text,
+                         "Registration page #1 — not passed")
+
+    def test_registration_page_2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element_by_xpath("//input[@class='form-control SeleniumPython' and @required]")
+        input1.send_keys("Ivan")
+
+        input2 = browser.find_element_by_xpath("//input[@class='form-control second' and @required]")
+        input2.send_keys("Petrov")
+
+        input3 = browser.find_element_by_xpath("//input[@class='form-control third' and @required]")
+        input3.send_keys("Smolensk")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", welcome_text,
+                         "Registration page #2 — not passed")
+
+
+if __name__ == '__main__':
+    unittest.main()
