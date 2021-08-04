2257d3125db3c3f1c52abbc46c820adbb099d327
White_Rabbit <lacrimozzy989wr>
2021-08-04 21:29:49 +0300

    nums

---
+++ first/01.py
@@ -0,0 +1,7 @@
+s = 'My Name is Julia'
+if 'is' in s:
+    print('Substring found')
+
+index = s.find('is')
+if index == 8:
+    print(f'Substring found at index {index}')
--- first/nums.py
+++ first/nums.py
@@ -6,5 +6,3 @@ def minus(a, b):
     return(a-b)
 if __name__ == '__main__':
     print(minus(3, 2))
-1123123
-123
